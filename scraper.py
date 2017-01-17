033a364f3ee76e8b39faced896e2ba0b93b4890d
Philip Guther <philip@guther.com>
2017-01-17 09:54:37 -0800

    moved content to scraper file and started Scraper class

--- scraper.py
+++ scraper.py
@@ -2,6 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 
 
+class Scraper(object):
+    """
+    Class to scrape a UCSC event
+    """
+
+
+class Event(object):
+    """
+    Object that represents a UCSC format Event
+    """
+
+    def __init__(self, title, description, location,
+                 location_details, admission, admission_details,
+                 sponsor, related_url, invited_audience,
+                 category, images, dates):
+        """
+        Initialize a new Event Object
+        """
+        self.title = title
+        self.description = description
+        self.location = location
+        self.location_details = location_details
+        self.admission = admission
+        self.admission_details = admission_details
+        self.sponsor = sponsor
+        self.related_url = related_url
+        self.invited_audience = invited_audience
+        self.category = category
+        self.images = images
+        self.dates = dates
+
+
 def get_soup_from_url(page_url):
         """
         Takes the url of a web page and returns a BeautifulSoup Soup object representation
